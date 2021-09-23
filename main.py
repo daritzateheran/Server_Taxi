809f7eb2ef73fd0fb3a022974549c18e89c59931
Daritza Teheran <daritzat@uninorte.edu.co>
2021-09-23 12:44:02 -0500

    polyline

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-from flask import Flask, render_template, jsonify, current_app
-from flask.cli import with_appcontext
+from flask import Flask, render_template, jsonify, g
 
-import socket, threading, pymysql, os, click
+import socket, threading, pymysql, os
 
 app = Flask(__name__)
 
 
-conn = pymysql.connect(
-        host=os.environ['FLASK_DATABASE_HOST'],
-        user=os.environ['FLASK_DATABASE_USER'],
-        password=os.environ['FLASK_DATABASE_PASSWORD'],
-        database=os.environ['FLASK_DATABASE']
+def get_conn():
+    if "conn" not in g:
+        g.conn = pymysql.connect(
+            host=os.environ['FLASK_DATABASE_HOST'],
+            user=os.environ['FLASK_DATABASE_USER'],
+            password=os.environ['FLASK_DATABASE_PASSWORD'],
+            database=os.environ['FLASK_DATABASE']
         )
-cur=conn.cursor()
-
-# ¿como probamos?
+        g.cur=g.conn.cursor()
+    return g.conn, g.cur
 
 def udp():
-
+    conn = pymysql.connect(
+            host=os.environ['FLASK_DATABASE_HOST'],
+            user=os.environ['FLASK_DATABASE_USER'],
+            password=os.environ['FLASK_DATABASE_PASSWORD'],
+            database=os.environ['FLASK_DATABASE']
+        )
+    cur=conn.cursor()
     try:
         server_udp = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         server_udp.bind(('0.0.0.0', 8051))
@@ -31,7 +37,6 @@ def udp():
             adrrs = server_address[1]
             print (f"La direccion IP del emisor: {adrrs}")
             print (f"El mensaje recibido es: {msg}")
-
             arr = msg.split(",")
             cur.execute("INSERT INTO datos (Latitud, Longitud, Fecha, Hora) VALUES (%s,%s,%s,%s)", (arr[1],arr[2],arr[3],arr[4]+arr[5]))
             conn.commit()   
@@ -40,16 +45,19 @@ def udp():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
 
 @app.route('/sqldata')
 def get_data():
+        conn, cur = get_conn()
         cur=conn.cursor()
         cur.execute("SELECT * FROM datos WHERE Id = (SELECT MAX(Id) FROM datos)")
         conn.commit() #si lo quito no sirve
         datos = cur.fetchall()
+        cur.close()
         print (datos)
         return jsonify(datos)
+        
         
 if __name__ == '__main__':
     server_udp = threading.Thread(target=udp, daemon=True)
